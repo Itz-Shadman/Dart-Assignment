945941d03b8a6ac6997bae4ad26ffee805f67b38
Shadman Chowdhury <chowdhuryshadman707@gmail.com>
2026-06-23 06:00:11 +0100

    Add files via upload

---
+++ Question1.dart
@@ -0,0 +1,52 @@
+import 'dart:io';
+
+// Write a program to print your name in Dart.
+/*
+void main(){
+  String name="Shaadman Rashid Chy";
+  print("My name is : $name");
+}
+*/
+
+// Write a program to print Hello I am “John Doe” and Hello I’am “John Doe” with single and double quotes.
+/*
+void main(){
+  String name1='Hello I am “John Doe”';
+  String name2="""Hello I am “John Doe”""";
+  print(name1);
+  print(name2);
+}
+*/
+
+// Declare constant type of int set value 7.
+/*
+void main(){
+  const value=7;
+  print("Value is constant : $value");
+}
+*/
+
+//Write a program in Dart that finds simple interest. Formula= (p * t * r) / 100
+/*
+void main() {
+  print("Enter p:");
+  int p = int.parse(stdin.readLineSync()!);
+  print("Enter t:");
+  int t = int.parse(stdin.readLineSync()!);
+  print("Enter r:");
+  int r = int.parse(stdin.readLineSync()!);
+  double interest = (p * t * r) / 100;
+  print("Interest is: $interest");
+}
+*/
+
+//Write a program to print a square of a number using user input.
+
+/*
+void main(){
+  print("Enter square:");
+  int p = int.parse(stdin.readLineSync()!);
+  int square=p*p;
+  print("Square is: $square");
+}
+*/
